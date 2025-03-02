92f4f46c227c9647d9f6fa47fcb1209a8f6d647b
Ilya Zadorin <greeenjou@gmail.com>
2025-03-02 17:08:21 +0500

    Created Second Class

---
+++ src/lesson1/SecondClass.java
@@ -0,0 +1,17 @@
+package lesson1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecondClass {
+    public static void main(String[] args) {
+    // Create a list and add three letters
+    List<Integer> numbers = new ArrayList<>();
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+
+    // Print the list
+    System.out.println("Numbers in the list: " + numbers);
+}
+}
