bcafd65fb8c2e259ff32e303f79e62657ae36f5b
Ilya Zadorin <greeenjou@gmail.com>
2025-03-02 17:01:33 +0500

    Created First Class

---
+++ src/lesson1/FirstLesson.java
@@ -0,0 +1,17 @@
+package lesson1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstLesson {
+    public static void main(String[] args) {
+    // Create a list and add three letters
+    List<String> letters = new ArrayList<>();
+    letters.add("A");
+    letters.add("B");
+    letters.add("C");
+
+    // Print the list
+    System.out.println("Letters in the list: " + letters);
+}
+}
