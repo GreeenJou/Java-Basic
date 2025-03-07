24fb307075af74d77f6ff879ef64aa3253b6d0bb
Ilya Zadorin <greeenjou@gmail.com>
2025-03-07 20:04:57 +0500

    Added homework about arithmetic operations

---
+++ src/lesson1/ThirdLesson.java
@@ -0,0 +1,18 @@
+package lesson1;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThirdLesson {
+    public static void main(String[] args) {
+        int A1 = 12;
+        int B2 = 16;
+        int C3= A1 + B2;
+        int V4 = A1 - B2;
+        int Z5 = A1 * B2;
+                System.out.println(C3);
+                System.out.println(V4);
+                System.out.println(Z5);
+    }
+}
